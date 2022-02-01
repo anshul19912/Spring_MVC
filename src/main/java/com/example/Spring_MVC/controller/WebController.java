9d33f00626cfe58dcb4dae1dca6de75ca7a5a813
Your Name <anshulsingh19912@gmail.com>
2022-02-01 14:48:08 +0530

    final commit

--- src/main/java/com/example/Spring_MVC/controller/WebController.java
+++ src/main/java/com/example/Spring_MVC/controller/WebController.java
@@ -1,8 +1,12 @@
 package com.example.Spring_MVC.controller;
 
+import com.example.Spring_MVC.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class WebController {
@@ -26,4 +30,17 @@ public class WebController {
         return "homepage";
     }
 
+    @RequestMapping(value = "/hello",method = POST)
+    public String sayHello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("USER_ID", name);  // key and value
+        model.addAttribute("name","JAVA");
+
+        Student student=new Student("Rahul",12,"Computer science");
+
+        model.addAttribute("Student",student);// key and value
+
+        return "hello";// returning hello.jsp view name
+
+
+    }
 }
